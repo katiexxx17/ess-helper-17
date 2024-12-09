11a340118fb24905d11a5a098d2932152e2b1f98
katiexxx17 <karstenekaterina1@gmail.com>
2024-12-09 14:30:48 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ document.querySelectorAll('.title-2-mcs').forEach(item => {
         info.style.display = info.style.display === 'none' || info.style.display === '' ? 'block' : 'none';
     });
 });
+
+document.getElementById('DonateButton').addEventListener('click', function() {
+    const reqs = document.getElementById('reqs');
+    if (reqs.style.display === 'none' || reqs.style.display === '') {
+        reqs.style.display = 'inline'; // Меняем на flex
+    } else {
+        reqs.style.display = 'none'; // Можно добавить возможность скрыть обратно
+    }
+});
+
+document.getElementsByClassName('open-cat').addEventListener('click', function() {
+    const reqs = document.getElementsByClassName('ustav-cat');
+    if (reqs.style.display === 'none' || reqs.style.display === '') {
+        reqs.style.display = 'flex'; // Меняем на flex
+    } else {
+        reqs.style.display = 'none'; // Можно добавить возможность скрыть обратно
+    }
+});
